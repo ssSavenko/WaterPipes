bfcebf4f742b82170a76c686628e521df2fa05bc
ss.savenko <ss.savenko@gmail.com>
2018-05-16 20:29:28 +0300

    save

--- WaterPipes/CommandsForGame/AddPipe.cs
+++ WaterPipes/CommandsForGame/AddPipe.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace WaterPipes.CommandsForCursor
 {
-    class AddPipe
+    internal class AddPipe : ICommandForGame
     {
+        Field fieldForChanges;
+        Cursor cursorForChanges;
+
+        public AddPipe(Field inputedField, Cursor inputedCursor)
+        {
+            fieldForChanges = inputedField;
+            cursorForChanges = inputedCursor;
+        }
+
+        public void performCommand(ConsoleKey inputedKey)
+        {
+            if (inputedKey == ConsoleKey.Enter)
+            {
+                fieldForChanges[cursorForChanges.YCordinate, cursorForChanges.XCordinate] = Cell.CellContainsPipe;
+            }
+        }
     }
 }
