ca34d33dc565cce73a72a14c44d06fa2ea713c1e
ss.savenko <ss.savenko@gmail.com>
2018-05-31 20:10:32 +0300

    Save

--- WaterPipes/GameIteration.cs
+++ WaterPipes/GameIteration.cs
@@ -11,85 +11,22 @@
 
         public bool ChangeField()
         {
-            bool IsFieldWereChanged = false;
-            bool[] WasPastStringChanged = new bool[fieldForChanges.Width];
-            bool WasPastCellChanged = false;
+            Field newField = new Field(fieldForChanges.Height, fieldForChanges.Width);
+            bool wasFieldChanged = false;
 
-            for (int i = 0; i < fieldForChanges.Height; i++)
+            for (int i =0; i < fieldForChanges.Height; i++)
             {
                 for (int j = 0; j < fieldForChanges.Width; j++)
                 {
-                    if (fieldForChanges[i, j] == Cell.CellContainsPipe)
+                    if (fieldForChanges[j, i] == Cell.CellContainsPipe)
                     {
-                        if (!WasPastStringChanged[j])
-                        {
-                            WasPastStringChanged[j] = false;
-                            WasPastCellChanged = false;
-
-                            if (i < fieldForChanges.Width - 1)
-                            {
-                                if (fieldForChanges[i + 1, j] == Cell.CellContainsPipeWithWater || fieldForChanges[i + 1, j] == Cell.CellContainsSource)
-                                {
-                                    fieldForChanges[i, j] = Cell.CellContainsPipeWithWater;
-                                    WasPastStringChanged[j] = true;
-                                    WasPastCellChanged = true;
-                                    IsFieldWereChanged = true;
-                                }
-                            }
-
-                            if (i != fieldForChanges.Height && i > 0)
-                            {
-                                if (!WasPastStringChanged[j])
-                                {
-                                    if (fieldForChanges[i - 1, j] == Cell.CellContainsPipeWithWater || fieldForChanges[i - 1, j] == Cell.CellContainsSource)
-                                    {
-                                        fieldForChanges[i, j] = Cell.CellContainsPipeWithWater;
-                                        WasPastStringChanged[j] = true;
-                                        WasPastCellChanged = true;
-                                        IsFieldWereChanged = true;
-                                    }
-                                }
-                            }
-                        }
-
-                        if (!WasPastCellChanged && fieldForChanges[i, j] != Cell.CellContainsPipeWithWater)
-                        {
-                            WasPastCellChanged = false;
-                            if (j != fieldForChanges.Width && j > 0)
-                            {
-                                if (!WasPastStringChanged[j])
-                                {
-                                    if (fieldForChanges[i, j - 1] == Cell.CellContainsPipeWithWater || fieldForChanges[i, j - 1] == Cell.CellContainsSource)
-                                    {
-                                        fieldForChanges[i, j] = Cell.CellContainsPipeWithWater;
-                                        WasPastStringChanged[j] = true;
-                                        WasPastCellChanged = true;
-                                        IsFieldWereChanged = true;
-                                    }
-                                }
-                            }
-                            if (j < fieldForChanges.Height - 1)
-                            {
-                                if (fieldForChanges[i, j + 1] == Cell.CellContainsPipeWithWater || fieldForChanges[i, j + 1] == Cell.CellContainsSource)
-                                {
-                                    fieldForChanges[i, j] = Cell.CellContainsPipeWithWater;
-                                    WasPastStringChanged[j] = true;
-                                    WasPastCellChanged = true;
-                                    IsFieldWereChanged = true;
-                                }
-                            }
-                        }
-
-                        if (fieldForChanges[i, j] == Cell.CellIsEmpty)
-                        {
-                            WasPastStringChanged[j] = false;
-                            WasPastCellChanged = false;
-                        }
+                        bool isNearSource = (fieldForChanges[j + 1, i] == Cell.CellContainsPipeWithWater || fieldForChanges[j + 1, i] == Cell.CellContainsSource);
+                        isNearSource = (fieldForChanges[j - 1, i] == Cell.CellContainsPipeWithWater || fieldForChanges[j + 1, i] == Cell.CellContainsSource) || isNearSource;
                     }
                 }
-                WasPastCellChanged = false;
             }
-            return IsFieldWereChanged;
+
+            return wasFieldChanged;
         }
     }
 }
