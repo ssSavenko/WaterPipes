4e65efec226dcbdf0f57ac83cdddc2c97418057b
ss.savenko <ss.savenko@gmail.com>
2018-06-02 22:41:49 +0300

    Close #5

--- WaterPipes/PrintableFieldWithCursor.cs
+++ WaterPipes/PrintableFieldWithCursor.cs
@@ -19,54 +19,12 @@ namespace WaterPipes
 
         public void PrintFieldWithCursor()
         {
-            for (int i = 0; i <= GetFieldForPrint.Height + 1; i++)
-            {
-                Console.ForegroundColor = ConsoleColor.Gray;
-                if (i == 0 || i == GetFieldForPrint.Height+1)
-                {
-                    for (int k = 0; k <= GetFieldForPrint.Width +1; k++)
-                    {
-                        Console.Write(symbolOfBounds);
-                    }
-                }
-                else
-                {
-                    for (int j = -1; j <= GetFieldForPrint.Width; j++)
-                    {
-                        if (j == -1 || j == GetFieldForPrint.Width)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            Console.Write(symbolOfBounds);
-                        }
-                        else if (cursorForPrint.XCordinate == j && cursorForPrint.YCordinate + 1 == i)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write(symbolOfCursor);
-                        }
-                        else if (GetFieldForPrint[i - 1, j] == Cell.CellContainsPipe)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            Console.Write(symbolOfCellWithPipe);
-                        }
-                        else if (GetFieldForPrint[i - 1, j] == Cell.CellContainsPipeWithWater)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            Console.Write(symbolOfCellWithPipe);
-                        }
-                        else if (GetFieldForPrint[i - 1, j] == Cell.CellContainsSource)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write(symbolOfSourceCell);
-                        }
-                        else if (GetFieldForPrint[i - 1, j] == Cell.CellIsEmpty)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            Console.Write(symbolOfEmptyCell);
-                        }
-                    }
-                }
-                Console.Write("\n");
-            }
+            int startCursorYPosition = Console.CursorTop + 1;
+            int startCursorXPosition = Console.CursorLeft + 1;
+            PrintField();
+
+            Console.SetCursorPosition(cursorForPrint.XCordinate + startCursorXPosition, cursorForPrint.YCordinate + startCursorYPosition);
+            Console.Write(symbolOfCursor);
         }
     }
 }
